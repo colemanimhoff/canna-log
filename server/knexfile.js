b36ef5a9d916c3192f82fc113f3ff592f25c5777
Coleman Imhoff <colemanimhoff@gmail.com>
2018-05-25 13:57:26 -0600

    updated knex file

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,5 +3,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/cannalog'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 }
