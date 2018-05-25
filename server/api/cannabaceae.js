ae07c35632c94c0d191e47e5fd42a5ae7cfb5f6e
Coleman Imhoff <colemanimhoff@gmail.com>
2018-05-25 13:01:25 -0600

    added getAll and getOne queries and routes

--- server/api/cannabaceae.js
+++ server/api/cannabaceae.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const queries = require('../db/queries')
+
+function isValidId(request, response, next) {
+    if(!isNaN(request.params.id)) return next()
+    next(new Error('Invalid Id'))
+}
 
 router.get('/', (request, response) => {
-    response.json({
-        message: 'Heyyyy'
+    queries.getAll().then(cannabis => response.json(cannabis))
+})
+
+router.get('/:id', isValidId, (request, response, next) => {
+    queries.getOne(request.params.id).then(cannabis => {
+        cannabis
+        ? response.json(cannabis)
+        : next()
     })
 })
 
--- server/db/queries.js
+++ server/db/queries.js
@@ -2,6 +2,9 @@ const knex = require('./knex')
 
 module.exports = {
     getAll() {
-        
+        return knex('cannabaceae')
+    },
+    getOne(id) {
+        return knex('cannabaceae').where('id', id).first()
     }
 }
